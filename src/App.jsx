b7bcf1d2cc7bd8a4c6303e8159fc1a552cd0f734
seifeldin15 <seifeldin02@outlook.com>
2024-12-04 07:02:06 +0200

    tailwind fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
-import './Global.css'
-
+import Signup from './pages/signup'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
