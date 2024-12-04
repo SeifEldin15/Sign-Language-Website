e67cabc3068e9887f05c0179a59f05b05569c1f8
seifeldin15 <seifeldin02@outlook.com>
2024-12-04 07:56:35 +0200

    home components

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import HeroSection from '../components/HeroSection';
+import ServicesSection from '../components/ServicesSection';
+import CommonWordsSlider from '../components/CommonWordsSlider';
 
 const Home = () => {
   return (
-   <>
-   </>
+    <div className="min-h-screen bg-gray-900 text-white">
+      <HeroSection />
+      <ServicesSection />
+      <CommonWordsSlider />
+    </div>
   );
 };
 
