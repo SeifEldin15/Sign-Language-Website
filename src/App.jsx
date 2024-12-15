3e5ee62205461bc3988664c57667039995463cc5
seifeldin15 <seifeldin02@outlook.com>
2024-12-15 05:53:07 +0200

    added all pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import { useState } from 'react'
 import Signup from './pages/signup'
 import Login from './pages/login'
 import Home from './pages/home'
+import Learn from './pages/learn'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Lesson from './pages/lesson'
+
+import 'font-awesome/css/font-awesome.min.css';
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -13,6 +18,8 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/learn" element={<Learn />} />
+        <Route path="/lesson" element={<Lesson />} />
       </Routes>
     </BrowserRouter>
     </>
