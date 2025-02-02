295ef588d8e4b0fa6f74c8a87bc316d81be44b22
seifeldin15 <seifeldin02@outlook.com>
2025-02-03 00:11:58 +0200

    Change button color

--- src/pages/QuestionPage.jsx
+++ src/pages/QuestionPage.jsx
@@ -182,8 +182,8 @@ const QuestionPage = () => {
                   </p>  
                   <button 
                     onClick={handleStartAnswer}
-                    className="bg-blue-500 text-white px-4 sm:px-6 py-2 rounded-lg text-sm sm:text-base">
-                    Start Answer
+                    className="bg-[#58cc02] text-white px-4 sm:px-6 py-2 rounded-lg text-sm sm:text-base">
+                    Start Answering
                   </button>
                 </div>
               ) : (
