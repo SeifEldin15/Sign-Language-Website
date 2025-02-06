a10f2e25c91d07b27d5df56a45d67f3ed52232e1
seifeldin15 <seifeldin02@outlook.com>
2025-02-06 23:13:02 +0200

    Edit question data

--- src/components/QuestionSection.jsx
+++ src/components/QuestionSection.jsx
@@ -1,18 +1,18 @@
 // components/QuestionSection.jsx
 import React from 'react';
-import signImage from '../assets/avatar/static_point_up.png';
+import signImage from '../assets/avatar/gifmaker_me (1).gif';
 
 const QuestionSection = () => {
   return (
     <div className="max-w-2xl mx-auto">
-      <h1 className="text-white text-3xl font-bold text-center mb-8">
-        Select the correct meaning
-      </h1>
       <div className="flex justify-center mb-12 relative">
         <div className="w-1/2">
           <img src={signImage} alt="Sign" className="w-full h-auto" />
         </div>
       </div>
+      <h2 className="text-white text-2xl font-bold mb-8">
+        What is the meaning of this sign?
+      </h2>
     </div>
   );
 };
